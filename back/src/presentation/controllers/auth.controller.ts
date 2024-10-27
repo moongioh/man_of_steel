aff23921d8a7617ca21a824153b9fd0d443ebb0c
suojae <suojae.developer@gmail.com>
2024-10-27 21:27:49 +0900

    feat: auth controller 추가

---
+++ back/src/presentation/controllers/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from '../../application/services/auth.service';
+import { Result } from '../../util/result';
+import { UserDTO } from '../dto/user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  public async login(
+    @Body() credentials: UserDTO,
+  ): Promise<Result<{ accessToken: string; refreshToken: string }>> {
+    return this.authService.login(credentials);
+  }
+
+  @Post('register')
+  public async register(@Body() user: UserDTO): Promise<Result<void>> {
+    return this.authService.register(user);
+  }
+
+  @Post('refresh')
+  public async refresh(
+    @Body() body: { userId: string; refreshToken: string },
+  ): Promise<Result<{ accessToken: string; refreshToken: string }>> {
+    return this.authService.refreshTokens(body.userId, body.refreshToken);
+  }
+
+  @Post('logout')
+  public async logout(
+    @Body() body: { userId: string; accessToken: string },
+  ): Promise<Result<void>> {
+    return this.authService.logout(body.userId, body.accessToken);
+  }
+}
