bf32144674a3c7b939eac726ff3567487c9acf35
suojae <suojae.developer@gmail.com>
2024-10-27 21:24:02 +0900

    Create user.dao.ts

---
+++ back/src/infrastructure/dao/user.dao.ts
@@ -0,0 +1,11 @@
+export class UserDAO {
+  public id: string;
+  public email: string;
+  public hashedPassword: string;
+
+  constructor(id: string, email: string, hashedPassword: string) {
+    this.id = id;
+    this.email = email;
+    this.hashedPassword = hashedPassword;
+  }
+}
