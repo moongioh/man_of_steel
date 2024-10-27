3528365ff437921ef2811efe7e52528f42c1f311
suojae <suojae.developer@gmail.com>
2024-10-27 21:28:00 +0900

    feat: 앱모듈에서 의존성주입

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './presentation/controllers/auth.controller';
+import { AuthService } from './application/services/auth.service';
+import { JWTService } from './application/services/jwt.service';
+import { UserRepository } from './infrastructure/repositories/user.repository';
+import { DBUserRepository } from './infrastructure/repositories/db-user.repository';
+import { CacheUserRepository } from './infrastructure/repositories/cache-user.repository';
+import { BcryptService } from './infrastructure/services/bcrypt.service';
+import { MySQLService } from './infrastructure/services/mysql.service';
+import { RedisService } from './infrastructure/services/redis.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    JWTService,
+    BcryptService,
+    MySQLService,
+    RedisService,
+    DBUserRepository,
+    CacheUserRepository,
+    {
+      provide: 'IUserRepository',
+      useClass: UserRepository,
+    },
+    {
+      provide: AuthService,
+      useFactory: (
+        userRepository: UserRepository,
+        bcryptService: BcryptService,
+        jwtService: JWTService,
+      ) => {
+        return new AuthService(userRepository, bcryptService, jwtService);
+      },
+      inject: ['IUserRepository', BcryptService, JWTService],
+    },
+  ],
 })
 export class AppModule {}
